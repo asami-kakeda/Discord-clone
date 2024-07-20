c7319d2b7378c5a325201897001c6705b2a77f07
“asami-kakeda” <a_kakeda@skyfall.co.jp>
2024-07-20 21:13:26 +0900

    サイドバーのstyle開始

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,14 +1,27 @@
 import React from "react";
 import "./Sidebar.scss";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      Sidebar;
       {/* sidebarLeft */}
-      <div className="sidebarLeft"></div>
+      <div className="sidebarLeft">
+        <div className="serverIcon">
+          <img src="./logo192.png" alt="" />
+        </div>
+        <div className="serverIcon">
+          <img src="./logo192.png" alt="" />
+        </div>
+      </div>
       {/* sidebarRight */}
-      <div className="sidebarRight"></div>
+      <div className="sidebarRight">
+        <div className="sidebarTop">
+          <h3>Discord</h3>
+          <ExpandMoreIcon />
+        </div>
+        {/* sidebarChannels */}
+      </div>
     </div>
   );
 };
