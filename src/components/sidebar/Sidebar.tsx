8f6897dd721255e09607c5cafa86835df327cd47
“asami-kakeda” <a_kakeda@skyfall.co.jp>
2024-07-20 17:47:38 +0900

    環境構築

---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Sidebar.scss";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      Sidebar;
+      {/* sidebarLeft */}
+      <div className="sidebarLeft"></div>
+      {/* sidebarRight */}
+      <div className="sidebarRight"></div>
+    </div>
+  );
+};
+
+export default Sidebar;
