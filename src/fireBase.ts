6afcd5af8e059ddaf9c9302b99eb92fe32b0b5d5
“asami-kakeda” <a_kakeda@skyfall.co.jp>
2024-07-21 18:01:50 +0900

    firebaseの導入

---
+++ src/fireBase.ts
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore/lite";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC1FlRK0K-T3qUUdPrKsk54i5S0VC9LSV8",
+  authDomain: "discord-clone-83e9e.firebaseapp.com",
+  projectId: "discord-clone-83e9e",
+  storageBucket: "discord-clone-83e9e.appspot.com",
+  messagingSenderId: "191284415261",
+  appId: "1:191284415261:web:955c472b4f8d37deadeff4",
+  measurementId: "G-MR4ZQY200C",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const db = getFirestore(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, provider, db };
