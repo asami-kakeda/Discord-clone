dde161314b8160493c4022221b24292f74fb1098
“asami-kakeda” <a_kakeda@skyfall.co.jp>
2024-07-21 14:25:23 +0900

    Discordチャンネルにスタイルを当てる

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -3,6 +3,9 @@ import "./Sidebar.scss";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import AddIcon from "@mui/icons-material/Add";
 import SidebarChannel from "./SidebarChannel";
+import MicIcon from "@mui/icons-material/Mic";
+import HeadphonesIcon from "@mui/icons-material/Headphones";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 const Sidebar = () => {
   return (
@@ -32,8 +35,24 @@ const Sidebar = () => {
             <AddIcon className="sidebarAddIcon" />
           </div>
 
-          <div className="sidebarChannellist"></div>
-          <SidebarChannel />
+          <div className="sidebarChannelList">
+            <SidebarChannel />
+          </div>
+
+          <div className="sidebarFooter">
+            <div className="sidebarAccount">
+              <img src="./icon.png" alt="" />
+              <div className="accountName">
+                <h4>Kakechin</h4>
+                <span>#11134</span>
+              </div>
+            </div>
+            <div className="sidebarVoice">
+              <MicIcon />
+              <HeadphonesIcon />
+              <SettingsIcon />
+            </div>
+          </div>
         </div>
       </div>
     </div>
