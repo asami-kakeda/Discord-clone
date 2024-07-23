75c085df86671c1cd47612707f00f2653333f95d
“asami-kakeda” <a_kakeda@skyfall.co.jp>
2024-07-23 10:46:25 +0900

    ログインページを作成

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,28 @@ import React from "react";
 import "./App.scss";
 import Sidebar from "./components/sidebar/Sidebar";
 import Chat from "./components/chat/Chat";
+import { useSelector } from "react-redux";
+import Login from "./components/Login/Login";
 
 function App() {
+  // const user = useSelector((state) => state.user.user);
+  const user = null;
+
   return (
     <div className="App">
-      {/* sidebar */}
-      <Sidebar />
+      {user ? (
+        <>
+          {/* sidebar */}
+          <Sidebar />
 
-      {/* chat */}
-      <Chat />
+          {/* chat */}
+          <Chat />
+        </>
+      ) : (
+        <>
+          <Login />
+        </>
+      )}
     </div>
   );
 }
