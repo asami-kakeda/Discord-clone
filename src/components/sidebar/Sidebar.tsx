bd629c04ec582adfb8daed789a58039c4959bbaa
“asami-kakeda” <a_kakeda@skyfall.co.jp>
2024-07-24 10:00:54 +0900

    チャンネルデータをDiscordに出力する

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Sidebar.scss";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import AddIcon from "@mui/icons-material/Add";
@@ -6,8 +6,42 @@ import SidebarChannel from "./SidebarChannel";
 import MicIcon from "@mui/icons-material/Mic";
 import HeadphonesIcon from "@mui/icons-material/Headphones";
 import SettingsIcon from "@mui/icons-material/Settings";
+import { auth, db } from "../../fireBase";
+import { useAppSelector } from "../../app/hooks";
+// import { collection, query } from "firebase/firestore/lite";
+import {
+  onSnapshot,
+  collection,
+  query,
+  DocumentData,
+} from "firebase/firestore";
+
+interface Channel {
+  id: string;
+  channel: DocumentData;
+}
 
 const Sidebar = () => {
+  const [channels, setChannels] = useState<Channel[]>([]);
+
+  const user = useAppSelector((state) => state.user);
+
+  const q = query(collection(db, "channels"));
+
+  //Firestoreからリアルタイムデータを取得しReactのステートに設定する
+  useEffect(() => {
+    onSnapshot(q, (querySnapshot) => {
+      const channelsResults: Channel[] = [];
+      querySnapshot.docs.forEach((doc) =>
+        channelsResults.push({
+          id: doc.id,
+          channel: doc.data(),
+        })
+      );
+      setChannels(channelsResults);
+    });
+  }, []);
+
   return (
     <div className="sidebar">
       {/* sidebarLeft */}
@@ -36,15 +70,17 @@ const Sidebar = () => {
           </div>
 
           <div className="sidebarChannelList">
-            <SidebarChannel />
+            {channels.map((channel) => (
+              <SidebarChannel />
+            ))}
           </div>
 
           <div className="sidebarFooter">
             <div className="sidebarAccount">
-              <img src="./icon.png" alt="" />
+              <img src={user?.photo} alt="" onClick={() => auth.signOut()} />
               <div className="accountName">
-                <h4>Kakechin</h4>
-                <span>#11134</span>
+                <h4>{user?.displayName}</h4>
+                <span>#{user?.uid.substring(0, 4)}</span>
               </div>
             </div>
             <div className="sidebarVoice">
