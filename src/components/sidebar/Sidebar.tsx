d1b4922b5d839a5fd8df2a5d4863c34e616219a1
“asami-kakeda” <a_kakeda@skyfall.co.jp>
2024-07-24 15:42:18 +0900

    discordチャンネルを追加する

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -9,11 +9,21 @@ import SettingsIcon from "@mui/icons-material/Settings";
 import { auth, db } from "../../fireBase";
 import { useAppSelector } from "../../app/hooks";
 import useCollection from "../../hooks/useCollection";
+import { addDoc, collection } from "firebase/firestore";
 // import { collection, query } from "firebase/firestore/lite";
 
 const Sidebar = () => {
   const user = useAppSelector((state) => state.user);
   const { documents: channels } = useCollection("channels");
+
+  const addChannel = async () => {
+    let channelName: string | null = prompt("新しいチャンネルを作成");
+    if (channelName) {
+      await addDoc(collection(db, "channels"), {
+        channelName: channelName,
+      });
+    }
+  };
 
   return (
     <div className="sidebar">
@@ -39,7 +49,7 @@ const Sidebar = () => {
               <ExpandMoreIcon />
               <h3>プログラミングチャンネル</h3>
             </div>
-            <AddIcon className="sidebarAddIcon" />
+            <AddIcon className="sidebarAddIcon" onClick={() => addChannel()} />
           </div>
 
           <div className="sidebarChannelList">
